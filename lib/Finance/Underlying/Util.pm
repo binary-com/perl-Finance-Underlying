af3afbfaefa814bf311f9b5320bb698b31698b4d
Binary-Mohanad <mohanad@binary.com>
2020-05-13 09:51:39 +0000

    crypto/perl_repos_support_longer_currencies

---
+++ lib/Finance/Underlying/Util.pm
@@ -0,0 +1,37 @@
+package Finance::Underlying::Util;
+
+use strict;
+use warnings;
+
+use base qw( Exporter );
+our @EXPORT_OK = qw( inverse_frx_symbol );
+
+# VERSION
+
+=head2 inverse_frx_symbol
+
+inverse the frx symbol using Regex
+knowing that, all frx symbols follow this regular expression /^(frx)([A-Z]{3})([A-Z]{3})$/
+
+=over 4
+
+=item <symbol> the underlying symbol
+
+=back
+
+return the inverted symbol or empty string if couldn't inverse the symbol 
+
+=cut
+
+sub inverse_frx_symbol {
+    my $symbol = shift;
+
+    if(my ($prefix, $asset, $quoted) = $symbol =~ /^(cry|frx)([A-Z]{3})([A-Z]{3})$/) {
+        my $inverted_symbol = $prefix . $quoted . $asset;
+        return $inverted_symbol;
+    }
+
+    return '';
+}
+
+1;
