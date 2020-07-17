e6bd8401353326400174e7206d6994e8bb0ac75b
keanfei-binary <keanfei@binary.com>
2020-07-17 02:08:20 +0000

    Added pip order precision for underlying

--- lib/Finance/Underlying.pm
+++ lib/Finance/Underlying.pm
@@ -48,11 +48,12 @@ Returns a list of all underlyings, ordered by symbol.
 
 =cut
 
-sub display_decimals {
-    my ($self, $value) = @_;
-    my $big_float_val = Math::BigFloat->new($value);
-    return abs($big_float_val->exponent() );
-}
+=head2 pip_order_precision
+
+Returns the order of precision for pip size of the underlying. 
+e.g. 0.001 returns 3
+
+=cut
 
 sub pipsized_value {
     my ($self, $value, $custom) = @_;
@@ -60,7 +61,7 @@ sub pipsized_value {
     my $display_decimals =
         $custom
         ? log(1 / $custom) / log(10)
-        : log(1 / $self->pip_size) / log(10);
+        : $self->pip_order_precision;
     if (defined $value and looks_like_number($value)) {
         $value = sprintf '%.' . $display_decimals . 'f', $value;
     }
@@ -71,11 +72,11 @@ sub all_underlyings {
     map { $underlyings{$_} } sort keys %underlyings;
 }
 
-sub pip_size_decimals {
-    my ($self) = @_;
+sub pip_order_precision {
+    my $self = shift;
     return log(1 / $self->pip_size) / log(10);
-
 }
+
 
 =head2 symbols
 
