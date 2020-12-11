14d61cd14ed0025258d21bdf09cb99b37d9f18bb
weisheng-binary <39893981+weisheng-binary@users.noreply.github.com>
2020-12-11 09:05:17 +0800

    Suggestion
    Co-authored-by: Greg Sabino Mullane <greg@binary.com>

--- lib/Finance/Underlying.pm
+++ lib/Finance/Underlying.pm
@@ -56,7 +56,7 @@ Returns the number of digits of the underlying pip size or a custom pip size
 sub pipsized_value {
     my ($self, $value, $custom) = @_;
     
-    my $pip_size = $custom ? $custom : $self->pip_size;
+    my $pip_size = $custom || $self->pip_size;
 
     if (defined $value and looks_like_number($value)) {
         $value = roundcommon($pip_size, $value);
